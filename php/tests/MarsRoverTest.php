881877dca17cb89011725d72dc7ddefbda4fda36
Nolwenn <donolwenn@gmail.com>
2021-11-17 21:46:32 +0100

    complete test coverage

--- php/tests/MarsRoverTest.php
+++ php/tests/MarsRoverTest.php
@@ -42,4 +42,40 @@ class MarsRoverTest extends TestCase
 
         $this->assertEquals("0:0:E", $result);
     }
+
+    public function testStuckInNorth()
+    {
+        $rover = new MarsRover(0,0, "N");
+
+        $result = $rover->execute("M", [[0,1]]);
+
+        $this->assertEquals("O:0:0:N", $result);
+    }
+
+    public function testStuckInSouth()
+    {
+        $rover = new MarsRover(0,0, "S");
+
+        $result = $rover->execute("M", [[0,9]]);
+
+        $this->assertEquals("O:0:0:S", $result);
+    }
+
+    public function testStuckInWest()
+    {
+        $rover = new MarsRover(0,0, "W");
+
+        $result = $rover->execute("M", [[9,0]]);
+
+        $this->assertEquals("O:0:0:W", $result);
+    }
+
+    public function testStuckInEast()
+    {
+        $rover = new MarsRover(0,0, "E");
+
+        $result = $rover->execute("M", [[1,0]]);
+
+        $this->assertEquals("O:0:0:E", $result);
+    }
 }
